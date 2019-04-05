27454cd105e8cf5aff31915f5d99ee8d84a20aa0
Cedricium <amaya.cedric@gmail.com>
2019-04-04 20:54:08 -0700

    Finish MVP requirements for this project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,6 +13,7 @@ class Person {
 
 class Instructor extends Person {
   constructor(props) {
+    super(props)
     this.speciality = props.speciality
     this.favLanguage = props.favLanguage
     this.catchPhrase = props.catchPhrase
@@ -29,6 +30,7 @@ class Instructor extends Person {
 
 class Student extends Person {
   constructor(props) {
+    super(props)
     this.previousBackground = props.previousBackground
     this.className = props.className
     this.favSubjects = props.favSubjects
@@ -49,15 +51,88 @@ class Student extends Person {
 
 class ProjectManager extends Person {
   constructor(props) {
+    super(props)
     this.gradClassName = props.gradClassName
     this.favInstructor = props.favInstructor
   }
 
   standUp(channel) {
-    console.log(`${this.name} announces to ${channel}, @channel standup times!​​​​​`)
+    console.log(`${this.name} announces to ${channel}, '@channel standup time!​​​​​'`)
   }
 
   debugsCode(student, subject) {
     console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`)
   }
 }
+
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  gender: 'male',
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+})
+
+const dani = new Instructor({
+  name: 'Dani',
+  location: 'Seattle',
+  age: 30,
+  gender: 'female',
+  favLanguage: 'Python',
+  specialty: 'Back-end',
+  catchPhrase: `Si se puede!`
+})
+
+const cedric = new Student({
+  name: 'Cedric',
+  age: 22,
+  location: 'Bay Area',
+  gender: 'male',
+  previousBackground: 'quality assurance',
+  className: 'WebPT5',
+  favSubjects: [
+    'computer science',
+    'history'
+  ],
+})
+
+const bob = new Student({
+  name: 'Bob',
+  age: 25,
+  location: 'Salt Lake City',
+  gender: 'male',
+  previousBackground: 'insurance',
+  className: 'WebPT5',
+  favSubjects: [
+    'world history',
+    'Spanish',
+    'PE'
+  ],
+})
+
+const monica = new ProjectManager({
+  name: 'Monica',
+  age: 27,
+  location: 'Ann Arbor',
+  gender: 'female',
+  gradClassName: 'CS10',
+  favInstructor: 'Marco',
+})
+
+// Person
+cedric.speak()
+
+// Instructor
+fred.demo('React.js')
+dani.grade(cedric, 'JavaScript')
+
+// Student
+bob.listSubjects()
+cedric.PRAssignment('JavaScript-IV')
+bob.sprintChallenge('Advanced CSS')
+
+// Project Manager
+monica.standUp('webpt5_monica')
+monica.debugsCode(cedric, 'Node.js')
